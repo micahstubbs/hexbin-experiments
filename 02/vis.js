82d9ab96b568423142cfaac031f0101d5938226b
Micah Stubbs <micah.stubbs@gmail.com>
2017-06-21 10:31:14 -0700

    sh lebab.sh

--- 02/vis.js
+++ 02/vis.js
@@ -1,42 +1,41 @@
-var svg = d3.select("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+const svg = d3.select("svg");
+const margin = {top: 20, right: 20, bottom: 30, left: 40};
+const width = +svg.attr("width") - margin.left - margin.right;
+const height = +svg.attr("height") - margin.top - margin.bottom;
+const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+const randomX = d3.randomNormal(width / 2, 80);
+const randomY = d3.randomNormal(height / 2, 80);
+const points = d3.range(2000).map(() => [randomX(), randomY()]);
 
-var randomX = d3.randomNormal(width / 2, 80),
-    randomY = d3.randomNormal(height / 2, 80),
-    points = d3.range(2000).map(function() { return [randomX(), randomY()]; });
-
-var color = d3.scaleSequential(d3.interpolateLab("white", "steelblue"))
+const color = d3.scaleSequential(d3.interpolateLab("white", "steelblue"))
     .domain([0, 20]);
 
-var hexbin = d3.hexbin()
+const hexbin = d3.hexbin()
     .radius(20)
     .extent([[0, 0], [width, height]]);
 
-var x = d3.scaleLinear()
+const x = d3.scaleLinear()
     .domain([0, width])
     .range([0, width]);
 
-var y = d3.scaleLinear()
+const y = d3.scaleLinear()
     .domain([0, height])
     .range([height, 0]);
 
-var hexes = g.append("g")
+const hexes = g.append("g")
     .attr("class", "hexagon")
   .selectAll("path")
   .data(hexbin(points))
   .enter().append("path")
     .attr("d", hexbin.hexagon())
-    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-    .attr("fill", function(d) { return color(d.length); })
+    .attr("transform", d => `translate(${d.x},${d.y})`)
+    .attr("fill", d => color(d.length))
     .style("opacity", 0.5);
 
-window.setInterval(function(){
-    var rand = Math.random()*100;
+window.setInterval(() => {
+    const rand = Math.random()*100;
     hexes.transition()
         .duration(1000)
-        .delay(function(d,i){ return 9*i; })
+        .delay((d, i) => 9*i)
         .attr("d", hexbin.hexagon(rand));
 }, 1000);
