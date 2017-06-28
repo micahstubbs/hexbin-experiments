6f580693817388b2398be4867f5ce592fcc38880
Micah Stubbs <micah.stubbs@gmail.com>
2017-06-28 09:58:45 -0700

    add WIP iteration 05 that will constrain the step size between the random input value for the Perlin noise function

---
+++ 05/README.md
@@ -0,0 +1,11 @@
+now with [Perlin Noise](https://p5js.org/reference/#/p5/noise) as the source of randomness
+
+this iteration seeds the noise generator with a Math.random() scaled to the `0.005-0.03` range recommended by the p5js docs
+
+as suggested by [this tweet](https://twitter.com/JanWillemTulp/status/877536575898472448) from [@JanWillemTulp](https://twitter.com/JanWillemTulp)
+
+an iteration on the block [Living hexagonal binning](https://bl.ocks.org/maartenzam/b4ca68fd4ed40908ff79e5a8e37ae194) from [@maartenzam](https://twitter.com/maartenzam)
+
+---
+
+Some life added to [hexagonal binning](https://bl.ocks.org/mbostock/4248145).
---
+++ 05/vis.js
@@ -0,0 +1,56 @@
+/* global d3 */
+
+const svg = d3.select('body').append('svg')
+  .attr('width', 960)
+  .attr('height', 600);
+
+const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+const width = +svg.attr('width') - margin.left - margin.right;
+const height = +svg.attr('height') - margin.top - margin.bottom;
+const g = svg.append('g')
+  .attr('transform', `translate(${margin.left},${margin.top})`);
+
+const p5 = new window.p5();
+
+const randomX = d3.randomNormal(width / 2, 80);
+const randomY = d3.randomNormal(height / 2, 80);
+const points = d3.range(2000).map(() => [randomX(), randomY()]);
+
+const color = d3
+  .scaleSequential(d3.interpolateLab('white', 'steelblue'))
+  .domain([0, 20]);
+
+const hexbin = d3.hexbin()
+  .radius(20)
+  .extent([[0, 0], [width, height]]);
+
+const x = d3.scaleLinear()
+  .domain([0, width])
+  .range([0, width]);
+
+const y = d3.scaleLinear()
+  .domain([0, height])
+  .range([height, 0]);
+
+const hexes = g.append('g')
+  .attr('class', 'hexagon')
+  .selectAll('path')
+    .data(hexbin(points))
+    .enter()
+    .append('path')
+      .attr('d', hexbin.hexagon())
+      .attr('transform', d => `translate(${d.x},${d.y})`)
+      .attr('fill', d => color(d.length))
+      .style('opacity', 0.5)
+      .style('stroke', '#000')
+      .style('stroke-width', '0.5px');
+
+
+window.setInterval(() => {
+  Math.random()
+  const rand = noise(xoff) * 100;
+  hexes.transition()
+    .duration(1000)
+    .delay((d, i) => 9 * i)
+    .attr('d', hexbin.hexagon(rand));
+}, 1000);
